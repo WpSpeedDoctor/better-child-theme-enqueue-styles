5c9210febf51075f0315a4dabadfd5bd0972d7c2
Jaro <12481969+jarowp@users.noreply.github.com>
2020-10-15 21:22:15 +0100

    Update custom-styles.php

--- custom-styles/custom-styles.php
+++ custom-styles/custom-styles.php
@@ -1,25 +1,29 @@
 <?php
+/*  
+ *    Author Jaro Kurimsky
+ *    Git: https://github.com/jarowp/better-child-theme-enqueue-styles
+ *
+ *    To activate script add to functions PHP of the child theme following line
+ *    require_once ( trailingslashit( get_theme_file_path() ) . 'custom-styles/custom-styles.php');
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
-
-/*
-    To activate script add to functions PHP of the child theme following line
-    require_once ( trailingslashit( get_theme_file_path() ) . 'custom-styles/custom-styles.php');
-*/
 
 require_once (__DIR__.'/custom-styles-functions.php');
 
 if ( !function_exists( 'load_my_styles' ) ){
     function load_my_styles() {
 
-        //disable main CSS
-        // wp_dequeue_style('astra-theme-css');
+        /*disable main CSS example of disabling Astra parent theme*/
+        //wp_dequeue_style('astra-theme-css');
         
 
-        //add custom main style
-        // enqueue_my_style('style-main','style.css');
+        /*Example of enqueuing child theme style.css + astra inline CSS example*/
+        //$astra__inline_css = apply_filters( 'astra_dynamic_theme_css', '' );
+        //wp_enqueue_style ('main-style', trailingslashit( get_stylesheet_directory_uri() ).'style.css');
+        //wp_add_inline_style( 'main-style', $astra__inline_css );
 
         create_css_file();
         
@@ -27,12 +31,11 @@ if ( !function_exists( 'load_my_styles' ) ){
         load_css('page'); 
         load_css('inline'); 
 
-        //load fonts
-        // enqueue_my_asset_style('proxmanova-font','proximanova.css');
-        
+        /*Example of enqueuing fonts*/
+
         // enqueue_my_asset_style( 'fontello' ,'fontello.css');      
 
-         // wp_enqueue_style( 'gfonts', 'https://fonts.googleapis.com/css?family=EB+Garamond:500|Rubik&display=swap');
+        // wp_enqueue_style( 'gfonts', 'https://fonts.googleapis.com/css?family=EB+Garamond:500|Rubik&display=swap');
 
         the_debug();
     }
